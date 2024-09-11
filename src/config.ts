8c30e69a29b21895bf59453b641785a45a50eceb
Qiuyu Luo <qiuyluo@gmail.com>
2024-09-11 12:28:52 -0500

    Change the backend url.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const BACKEND_BASE_URL =
   process.env.NODE_ENV === 'production'
-      ? 'https://your-production-backend-url.com'
+      ? 'https://localhost:8080'
       : process.env.REACT_APP_BACKEND_URL;
       
 export { BACKEND_BASE_URL };

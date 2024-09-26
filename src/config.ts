9b65278991010009b2592330f90335b34881979f
Qiuyu Luo <qiuyluo@gmail.com>
2024-09-26 15:49:54 -0500

    Config backend url

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
-const BACKEND_BASE_URL =
-  process.env.NODE_ENV === 'production'
-      ? 'https://localhost:8080'
-      : process.env.REACT_APP_BACKEND_URL;
+// const BACKEND_BASE_URL =
+//   process.env.NODE_ENV === 'production'
+//       ? 'https://localhost:8080'
+//       : process.env.REACT_APP_BACKEND_URL;
+
+const BACKEND_BASE_URL = 'https://localhost:8080';
       
 export { BACKEND_BASE_URL };

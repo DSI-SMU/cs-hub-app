6c0f2059d7af98594526c15396f7c282a5f75ce8
francescalamarca <123484460+francescalamarca@users.noreply.github.com>
2024-09-27 15:36:34 -0500

    team 6

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -44,7 +44,7 @@ const Header: React.FC = () => {
         <header className={classes.header}>
             <span>
                 <a href="https://www.nasa.gov/nex" target="_blank">
-                    <img src="/images/SMU_logo.png" alt="NASA Logo" className={classes.logo} />
+                    <img src="/images/SMU_logo.png" alt="SMU Logo" className={classes.logo} />
                 </a>
             </span>
             <div className={classes.navItems}>
